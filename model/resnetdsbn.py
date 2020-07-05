72b09f14b90b61e80aa6cceb98854a5ea5b47a46
sally20921 <sally20921@github.com>
2020-07-06 08:36:37 +0900

    first commit today

--- model/resnetdsbn.py
+++ model/resnetdsbn.py
@@ -3,4 +3,7 @@ import math
 import torch.utils.model_zoo as model_zoo
 import torch.nn.functional as F
 
+def conv3x3(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
 
