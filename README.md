11c44cdb3c5daccba7df08ecbf13510f5bf2a937
sally20921 <sally20921@github.com>
2020-07-07 10:11:25 +0900

    first commit today

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Domain-Specific Batch Normalization 
 
+### modulelist 
+- torch.nn.ModuleList(modules: Optional[Iterable[torch.nn.modules.module.Module]] = None)
 
+- holds submodules in a list
+- ModuleList  can be  indexed like a regular Python list, but modules it contains are properly registered, and will be visible by all Module methods. 
--- model/dsbn.py
+++ model/dsbn.py
@@ -1,8 +1,14 @@
 from torch import nn
 
 class _DomainSpecificBatchNorm(nn.Module):
-    def __init__(self, num_features, num_classes, eps=1e-5, momentum=0.1):
+    def __init__(self, num_features, num_classes, eps=1e-5, momentum=0.1, affine=True, track_running_stats=True):
         super(_DomainSpecificBatchNorm, self).__init__()
+        self.bns = nn.ModuleList(
+                [nn.BatchNorm2d(num_features, eps, momentum, affine, track_running_stats) for _  in  range(num_classes)])
 
     def forward(self, x, domain_label):
+        bn =  self.bns[domain_label[0]]
+        return  bn(x), domain_label
+
+class DomainSpecificBatchNorm2d(_DomainSpecificBatchNorm):
 
